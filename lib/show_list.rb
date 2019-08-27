cb8ac847eb7df034f617980face25812b23291e1
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-08-27 15:51:20 -0700

    initial commit

---
+++ lib/show_list.rb
@@ -0,0 +1,9 @@
+require_relative "show_list/version"
+require_relative "show_list/api"
+require_relative "show_list/cli"
+require_relative "show_list/show"
+
+module ShowList
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/show_list/version.rb
@@ -0,0 +1,3 @@
+module ShowList
+  VERSION = "0.1.0"
+end
